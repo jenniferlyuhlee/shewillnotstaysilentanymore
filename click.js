36e608177a55af8ebfad8ae263655e95d82f2eb0
Glitch (hello-webpage) <none>
2021-04-15 05:51:58 +0000

    🚝👻 Checkpoint ./click.js:30726734/253 ./index.html:30726734/98

--- click.js
+++ click.js
@@ -2,12 +2,22 @@ var diam =20;
 var strokeColor=(0, 0, 0);
 var isGrowing=true;
 var centX, centY;
+bgImg=loadImage('asset/tw cover.png');
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   frameRate(24);
   smooth();
-  background
+  image(bgImg, 0, 0, width, height);
   centX=width/2;
-  centY
+  centY=height/2
+  strokeWeight(10);
+  fill(255, 25);
+}
+
+function draw(){
+  if(diam<=400){
+    ellipse(centX, centY, diam, diam);
+    diam+=20;
+  }
 }
