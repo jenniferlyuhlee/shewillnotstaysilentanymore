e74d9120c2d10edd3f05e015bae0543357d7e4d1
Glitch (hello-webpage) <none>
2021-04-15 05:39:59 +0000

    💾👨‍❤️‍👨 Checkpoint ./style.css:30726734/89 ./click.js:30726734/257

---
+++ click.js
@@ -0,0 +1,13 @@
+var diam =20;
+var strokeColor=(0, 0, 0);
+var isGrowing=true;
+var centX, centY;
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+  frameRate(24);
+  smooth();
+  background
+  centX=width/2;
+  centY
+}
