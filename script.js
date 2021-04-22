45f5fb75224a8d703321b3a172a71ad5cbdb0c58
Glitch (hello-webpage) <none>
2021-04-22 02:13:50 +0000

    👂🐾 Checkpoint ./main.js:30726734/3192 ./script.js:30726734/7088 ./home.html:30726734/40 ./involved.html:30726734/303

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+var t;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  stroke(255, 214, 0, 15);
+  noFill();
+  t = 0;
+}
+
+function draw() {
+  var x1 = mouseX;
+  var x2 = width * noise(t + 20);
+  var x3 = width * noise(t + 30);
+  var x4 = width * noise(t + 40);
+  var y1 = mouseY;
+  var y2 = height * noise(t + 50);
+  var y3 = height * noise(t + 60);
+  var y4 = height * noise(t + 70);
+
+  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  t += 0.005;
+
+  if (frameCount % 1000 == 0) {
+    background(255);
+  }
+}
+
