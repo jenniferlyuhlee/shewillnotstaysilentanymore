d524f419fb5b33bd62d5bed082ad5c7e317af324
Glitch (hello-webpage) <none>
2021-04-22 08:59:05 +0000

    😹🚘 Checkpoint ./bg.js:30726734/2259 ./involved.html:30726734/27 ./style.css:30726734/347

--- bg.js
+++ bg.js
@@ -1,50 +1,64 @@
-
-var x2 = 0;
-var y2 = 0;
-
+var myFlower
+var flowers= []
 function setup() {
-  createCanvas(800, 800);
-  background(255);
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	for (var i=0;i<15;i++){
+		let flower = generateRandomFlower()
+		flowers.push(flower)
+	}	
 }
 
+function drawFlower(flower){
+	push();
+		translate(flower.x,flower.y);
+		rotate(flower.size);
+		fill(255, 214, 0);
+		strokeWeight(1.5);
+		ellipse(0,0,50);
+		ellipseMode(CORNER)
+		fill(255, 253, 240);
+		stroke(237, 235, 218);
+	  if (flower.color){
+			fill(flower.color);
+		}
+		for (var i=0;i<30;i++){
+			ellipse(20,-20,60*flower.size,20);
+			rotate(PI/12);
+		}
+	pop()
+}
+var list =["#fcfce3","#ffa6a6","#bbb3ff","#f9ff99","#ff8a66","#cea3e3", "#d1ff9c"]
+var sizes = [1,0.9,1.1,0.9,0.8]
+function generateRandomFlower(x,y){
+	return{
+		x: x ||random(width),	
+		y: y ||random(height),
+		size:random(sizes) ,
+		color: random(list)
+	}
+}
+
+function mousePressed(){
+	var flower = generateRandomFlower(mouseX,mouseY)
+	flowers.push(flower)
+}
 
 function draw() {
-  for (let x=20; x<=width+100; x+=56){
-    for (let y=10; y<=height+100; y+=56){
-      
-      //red shade changer
-      var move = abs(mouseX-x);
-      strokeWeight(2);
-      stroke(move, 0, 0);
-      
-      //OCTAGON
-      //polygon template, modified no. of sides
-      //adapted from https://p5js.org/examples/form-regular-polygon.html
-      let angle = TWO_PI / 8;           
-      beginShape();
-      for (let a = 0; a < TWO_PI; a += angle) {
-        let sx = x + cos(a) * 28;
-        let sy = y + sin(a) * 28;
-        vertex(sx, sy);
-      }
-      endShape(CLOSE);
-      
-      //details inside octogon, wheel
-      ellipse(x, y, 45, 45);
-      rectMode(CENTER);
-      rect(x, y, 30, 30);
-      for (let w = 0; w<=10; w+=5){
-        //horizontal weave
-        line(x-15, y-w, x, y-w);
-        line(x+15, y+w, x, y+w);
-        //vertical weave
-        line(x+w, y, x+w, y-15);
-        line(x-w, y, x-w, y+15);
-      }
-    }
-  }
+	
+	background(255);
+	
+	for (var i=0;i<flowers.length;i++){
+		let flower = flowers[i]
+		drawFlower(flower)
+	
+		if (dist(mouseX,mouseY,flower.x,flower.y)<200){
+			flower.size=lerp(flower.size,2,0.1)
+		}else{
+			flower.size=lerp(flower.size,0.8,0.05)
+		}
+		
+	}
+	
+
 }
-
-
-
-
