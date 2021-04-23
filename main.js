a4717f6031489242e8f3ad33d1adb22617afaba4
Glitch (hello-webpage) <none>
2021-04-23 00:15:24 +0000

    🚟📒 Checkpoint ./main.js:30726734/675 ./style.css:30726734/137

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
+/*mission page*/
+
+$(document).ready(function () {   
+     $('.voice').click(function () {
+        $('.text').toggle(3000);     
+        $(this).toggleClass("bigger");
+    });
+    
+});
+
 $(document).ready(function () {
-    $(function () {
-        $(".sticky").draggable();
-    });   
+    $('.mission').css('background', 'linear-gradient(0deg, rgba(255,255,255,0) 50%, rgba(255, 214, 0, 0.5) 100%)');
 });
 
 
 
-$(document).ready(function () {   
-     $('.voice').click(function () {
-        $('.text').toggle(2000);     
-        $(this).toggleClass("bigger");
-    });
-    
+/*archives page*/
+$(document).ready(function () {
+    $(function () {
+        $(".sticky").draggable();
+    });   
 });
 
 
