c8aed3f16d70e284246d901d88f403ca0be2ff1d
Glitch (hello-webpage) <none>
2021-04-24 01:58:21 +0000

    🍐🐒 Checkpoint ./main.js:30726734/406

--- main.js
+++ main.js
@@ -21,3 +21,25 @@ $(document).ready(function () {
 });
 
 
+
+/*memorials page*/
+$(document).ready(function(){
+
+	//------------------------------------
+	// show hide each itme
+	//------------------------------------
+
+	$('.item').click(function(){
+		$(this).toggleClass('active');
+	});
+
+	//---------------------------
+	// setup show hide all button
+	//---------------------------
+	$("#showhide").click(function(){
+		
+		$('.item').toggleClass('active');
+	});
+
+});
+
