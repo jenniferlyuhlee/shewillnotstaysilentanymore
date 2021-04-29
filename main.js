a51116ba9e57201c9cfae668ac5a8fda81083398
Glitch (hello-webpage) <none>
2021-04-29 22:27:28 +0000

    🌦⛲️ Checkpoint ./about.html:30726734/8738 ./style.css:30726734/456 ./main.js:30726734/201

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*about page*/
+$(document).ready(function () {
+    $('.about').css('background', 'linear-gradient(0deg, rgba(255,255,255,0) 50%, rgba(255, 214, 0, 0.5) 100%)');
+});
+
+
 /*mission page*/
 
 $(document).ready(function () {   
